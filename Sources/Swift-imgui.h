6715d13e179e915d262d2e236e2595eb3a5e9ab7
mnmly <i.am@mnmly.com>
2017-05-12 20:16:43 -0700

    Initial commit

---
+++ Sources/Swift-imgui.h
@@ -0,0 +1,19 @@
+//
+//  Swift-imgui.h
+//  Swift-imgui
+//
+//  Created by Hiroaki Yamane on 5/12/17.
+//  Copyright © 2017 Hiroaki Yamane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swift-imgui.
+FOUNDATION_EXPORT double Swift_imguiVersionNumber;
+
+//! Project version string for Swift-imgui.
+FOUNDATION_EXPORT const unsigned char Swift_imguiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swift_imgui/PublicHeader.h>
+
+
