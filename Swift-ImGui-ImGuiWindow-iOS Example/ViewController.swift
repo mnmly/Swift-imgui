7ac8ffb32241bb7cb2c148d2b5fbbe5f74ef9f52
mnmly <i.am@mnmly.com>
2018-08-01 15:10:17 -0700

    Adjusted example

--- Swift-ImGui-ImGuiWindow-iOS Example/ViewController.swift
+++ Swift-ImGui-ImGuiWindow-iOS Example/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
         
         let v = UIView(frame: CGRect(origin: view.center, size: CGSize(width: 50, height: 50)))
         v.center = view.center
-        v.backgroundColor = UIColor.lightGray
+        v.backgroundColor = UIColor.lightGray.withAlphaComponent(0.4)
         view.addSubview(v)
         
         ImGui.draw { (imgui) in
@@ -32,10 +32,12 @@ class ViewController: UIViewController {
                 DispatchQueue.main.async {
                     let center = v.center
                     let propAnimator = UIViewPropertyAnimator(duration: TimeInterval(Temp.duration), dampingRatio: Temp.dampingRatio, animations: {
-                        v.center = CGPoint(x: center.x, y: center.y + 100.0)
+                        v.center = CGPoint(x: center.x, y: center.y - 100.0)
                     })
                     propAnimator.addCompletion({ (p) in
-                        v.center = center
+                        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false, block: { (_) in
+                            v.center = center
+                        })
                     })
                     propAnimator.startAnimation()
                 }
